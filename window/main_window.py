79885bf7ef8108e874ffedb0d734ea90aef1d673
RayK <zrztt@outlook.com>
2022-04-14 01:35:36 +0800

    优化代码结构

--- window/main_window.py
+++ window/main_window.py
@@ -8,19 +8,21 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__(parent)
         self.setupUi(self)
         self.init_ui()
-
-        # 按钮绑定
-        self.filter_button.clicked.connect(self.filter_button_clicked)
+        self.bind_button()
 
     # 初始化页面
     def init_ui(self):
         # 默认隐藏筛选器
         self.filter_frame.setVisible(False)
 
+    # 按钮绑定
+    def bind_button(self):
+        # 筛选按钮
+        self.filter_button.clicked.connect(self.filter_button_clicked)
+
+    # 筛选按钮点击 折叠/展开
     def filter_button_clicked(self):
         if self.filter_frame.isVisible():
-            # self.filter_frame.setVisible(False)
             self.filter_frame.hide_and_show(False)
         else:
-            # self.filter_frame.setVisible(True)
             self.filter_frame.hide_and_show(True)
